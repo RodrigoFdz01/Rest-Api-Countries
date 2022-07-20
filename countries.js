5085759683670b2736915ab24cbd9caa5e7164d6
RodrigoFdz01 <rodrigof3012@gmail.com>
2022-07-20 17:49:11 -0300

    input search..

--- countries.js
+++ countries.js
@@ -1,31 +1,44 @@
 let divContainer = document.getElementById("container2");
-let input = document.querySelector("input");
+let input = document.getElementById("input");
+let botonInput = document.getElementById("basic-addon2");
 
-const url = "https://restcountries.com/v2/all";
-fetch(url)
-  .then((resp) => resp.json())
-  .then(function (data) {
-    //console.log(data);
-    data.map((item, index) => {
-      const inputValue = input.value.toLowerCase();
-      if (inputValue === "") {
-        const div = document.createElement("div");
-        div.classList.add("card");
+input.addEventListener("keyup", (event) => {
+  console.log(event.target.value);
+  event.preventDefault();
+  search();
+});
 
-        div.innerHTML = `<h3>${index + 1}-Country: ${item.name}</h3>  
-        <h4>Capital: ${item.capital}</h4>
-        <p>Region: ${item.region}</p>
-        <img class="img" src=${item.flag} alt="">`;
-        divContainer.appendChild(div);
-      }
-      const name = item.name.toLowerCase();
-      if (name.indexOf(inputValue) !== -1) {
-        //console.log("2do if");
-        const div = document.createElement("div");
-        div.innerHTML = `<h2>${index + 1}-Country: ${item.name}</h2>`;
-      }
+const search = () => {
+  //console.log("disparado...");
+  const url = "https://restcountries.com/v2/all";
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      //console.log(data);
+      data.map((item) => {
+        const inputValue = input.value.toLowerCase();
+
+        if (inputValue == "") {
+          const div = document.createElement("div");
+          div.classList.add("card");
+
+          div.innerHTML = `<h3>Country: ${item.name}</h3>  
+          <h4>Capital: ${item.capital}</h4>
+          <p>Region: ${item.region}</p>
+          <img class="img" src=${item.flag} alt="">`;
+          divContainer.appendChild(div);
+        }
+
+        const name = item.name.toLowerCase();
+
+        if (name.indexOf(inputValue) !== -1) {
+          //console.log("2do if");
+          const div = document.createElement("div");
+          div.innerHTML += `<h2>Country: ${item.name}</h2>`;
+        }
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+};
