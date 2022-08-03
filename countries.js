53a8a704c8144da9dde6a2d0d0595e00461212bc
RodrigoFdz01 <rodrigof3012@gmail.com>
2022-08-03 18:56:07 -0300

    filter

--- countries.js
+++ countries.js
@@ -2,39 +2,28 @@ let divContainer = document.getElementById("container2");
 let input = document.getElementById("input");
 let botonInput = document.getElementById("basic-addon2");
 
-input.addEventListener("keyup", (event) => {
-  console.log(event.target.value);
+botonInput.addEventListener("click", (event) => {
   event.preventDefault();
-  search();
+  byName();
 });
 
+const url = "https://restcountries.com/v2/all";
+
 const search = () => {
-  //console.log("disparado...");
-  const url = "https://restcountries.com/v2/all";
   fetch(url)
     .then((resp) => resp.json())
     .then(function (data) {
-      //console.log(data);
       data.map((item) => {
-        const inputValue = input.value.toLowerCase();
-
-        if (inputValue == "") {
+        // const inputValue = input.value.toLowerCase();
+        const name = item.name.toLowerCase();
+        if (name.indexOf(inputValue) != -1) {
           const div = document.createElement("div");
           div.classList.add("card");
-
           div.innerHTML = `<h3>Country: ${item.name}</h3>  
           <h4>Capital: ${item.capital}</h4>
           <p>Region: ${item.region}</p>
           <img class="img" src=${item.flag} alt="">`;
           divContainer.appendChild(div);
-        }
-
-        const name = item.name.toLowerCase();
-
-        if (name.indexOf(inputValue) !== -1) {
-          //console.log("2do if");
-          const div = document.createElement("div");
-          div.innerHTML += `<h2>Country: ${item.name}</h2>`;
         }
       });
     })
@@ -42,3 +31,41 @@ const search = () => {
       console.log(error);
     });
 };
+//search();
+
+const byName = () => {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      const inputValue = input.value.toLowerCase();
+      data.filter((element) => element.name == inputValue);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+// if (inputValue != "") {
+//   if (name.indexOf(inputValue) != -1) {
+//     const div2 = document.createElement("div");
+//     div2.classList.add("card");
+//     div2.innerHTML = `<h3>Country: ${item.name}</h3>
+//   <h4>Capital: ${item.capital}</h4>
+//   <p>Region: ${item.region}</p>
+//   <img class="img" src=${item.flag} alt="">`;
+//     divContainer.appendChild(div2);
+//   }
+// }
+
+/*
+.map((item) => {
+  const div = document.createElement("div");
+  //console.log(data.name);
+  div.classList.add("card");
+  div.innerHTML = `<h3>Country: ${item.name}</h3>  
+  <h4>Capital: ${item.capital}</h4>
+  <p>Region: ${item.region}</p>
+  <img class="img" src=${item.flag} alt="">`;
+  divContainer.appendChild(div);
+});
+*/
