a16e0feb8da2f33877269091612c161038a7aa14
RodrigoFdz01 <rodrigof3012@gmail.com>
2022-08-04 16:41:04 -0300

    filter2

--- countries.js
+++ countries.js
@@ -4,26 +4,44 @@ let botonInput = document.getElementById("basic-addon2");
 
 botonInput.addEventListener("click", (event) => {
   event.preventDefault();
-  byName();
+  //byName2();
 });
 
 const url = "https://restcountries.com/v2/all";
 
-const search = () => {
+const searchAll = () => {
   fetch(url)
     .then((resp) => resp.json())
     .then(function (data) {
       data.map((item) => {
-        // const inputValue = input.value.toLowerCase();
-        const name = item.name.toLowerCase();
-        if (name.indexOf(inputValue) != -1) {
-          const div = document.createElement("div");
-          div.classList.add("card");
-          div.innerHTML = `<h3>Country: ${item.name}</h3>  
+        const div = document.createElement("div");
+        div.classList.add("card");
+        div.innerHTML = `<h3>Country: ${item.name}</h3>  
           <h4>Capital: ${item.capital}</h4>
           <p>Region: ${item.region}</p>
           <img class="img" src=${item.flag} alt="">`;
-          divContainer.appendChild(div);
+        divContainer.appendChild(div);
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+//searchAll();
+
+/*
+const byName = () => {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      data.map((el) => {
+        const inputValue = input.value.toLowerCase();
+        console.log(inputValue);
+        const name = el.name;
+        if (name == inputValue) {
+          console.log(name);
+          console.log("si");
         }
       });
     })
@@ -31,36 +49,35 @@ const search = () => {
       console.log(error);
     });
 };
-//search();
+*/
 
-const byName = () => {
+const byName2 = () => {
+  const inputValue = input.value.toLowerCase();
   fetch(url)
     .then((resp) => resp.json())
     .then(function (data) {
-      const inputValue = input.value.toLowerCase();
-      data.filter((element) => element.name == inputValue);
+      data.map((item) => {
+        const name = item.name;
+        if (name.indexOf(inputValue) != -1) {
+          const div2 = document.createElement("div");
+          div2.classList.add("card");
+          div2.innerHTML = `<h3>Country: ${item.name}</h3>
+                               <h4>Capital: ${item.capital}</h4>
+                               <p>Region: ${item.region}</p>
+                               <img class="img" src=${item.flag} alt="">`;
+          divContainer.appendChild(div2);
+        }
+      });
     })
     .catch(function (error) {
       console.log(error);
     });
 };
 
-// if (inputValue != "") {
-//   if (name.indexOf(inputValue) != -1) {
-//     const div2 = document.createElement("div");
-//     div2.classList.add("card");
-//     div2.innerHTML = `<h3>Country: ${item.name}</h3>
-//   <h4>Capital: ${item.capital}</h4>
-//   <p>Region: ${item.region}</p>
-//   <img class="img" src=${item.flag} alt="">`;
-//     divContainer.appendChild(div2);
-//   }
-// }
-
 /*
 .map((item) => {
   const div = document.createElement("div");
-  //console.log(data.name);
+
   div.classList.add("card");
   div.innerHTML = `<h3>Country: ${item.name}</h3>  
   <h4>Capital: ${item.capital}</h4>
